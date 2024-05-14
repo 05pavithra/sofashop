fa2e37f92baeaa9241e4972aece9d09d4ada3072
05pavithra <94049154+05pavithra@users.noreply.github.com>
2024-05-14 13:46:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Shopsec from './Components/Shopsec';
+import Aboutus from './Components/Aboutus';
+import Services from './Components/Services';
+import Rblog from './Components/Rblog';
+import Contact from './Components/Contact';
+import Login from './Components/Login';
+import Signin from './Components/Signin';
+import { useState } from 'react';
+import Newlogin from './Components/Newlogin';
+
+
+function App() {
+  const [user,setuser] = useState(null)
+  return (
+      <div>
+               {/* <Route path='/Login' element={<Login user={user} setuser={setuser}/>}/> */}
+
+        <BrowserRouter>
+        
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+       {/* <Route path='/' element={user?<Home/>:<Navigate to='/Newlogin'/>}/> */}
+
+       <Route path='/shop' element={<Shopsec/>}/>
+       <Route path='/about' element={<Aboutus/>}/>
+       <Route path='/services' element={<Services/>}/>       
+       <Route path='/Rblog' element={<Rblog/>}/>
+       <Route path='/contact' element={<Contact/>}/>
+       {/* <Route path='/Newlogin' element={<Newlogin user={user} setuser={setuser}/>}/> */}
+       {/* <Route path='/Signin' element={<Signin/>}/> */}
+        </Routes>
+        </BrowserRouter>
+       
+      </div>
+      );
+}
+
+export default App;
